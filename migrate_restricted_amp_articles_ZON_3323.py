51a46c7d449032a55093628467a02ca0b755d1e2
Florian Pilz <fp@gocept.com>
2016-10-13 14:47:01 +0200

    ZON-3323: Fix migration for `is_amp` if `access` is not set.

--- migrate_restricted_amp_articles_ZON_3323.py
+++ migrate_restricted_amp_articles_ZON_3323.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
     migration_helper = migrate_properties.PropertyMigrationHelper()
     for uniqueId in sys.stdin:
         with migration_helper.properties(uniqueId.strip()) as props:
-            if props['{%s}access' % namespace] != 'free':
+            access = '{%s}access' % namespace
+            if access in props and props[access] != 'free':
                 props['{%s}is_amp' % namespace] = 'no'
